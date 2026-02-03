ce2f42fbb43efffc6fe28ecd3b1c4301fcfe3fe2
Andrei Tuicu <tuicu@adobe.com>
2026-02-03 10:00:05 -0800

    feat: Don't instrument the HTML when in live preview

--- src/handlers/get.js
+++ src/handlers/get.js
@@ -46,6 +46,10 @@ export default async function getHandler({ req, env, daCtx }) {
     return get404();
   }
 
+  if (new URL(req.url).searchParams.get('dapreview') === 'on') {
+    return handleAEMProxyRequest({ req, env, daCtx });
+  }
+
   // default route to DA admin for all the content requests
   return daSourceGet({ req, env, daCtx });
 }
