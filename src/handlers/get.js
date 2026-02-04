ed2567ed9c5f4da064b84fbf34a2f45c427c3122
Andrei Tuicu <andrei.tuicu@gmail.com>
2026-02-04 11:50:23 -0800

    fix: Fragments were UE instrumented (#149)

--- src/handlers/get.js
+++ src/handlers/get.js
@@ -46,7 +46,10 @@ export default async function getHandler({ req, env, daCtx }) {
     return get404();
   }
 
-  if (new URL(req.url).searchParams.get('dapreview') === 'on') {
+  const url = new URL(req.url);
+  const isPreviewHost = url.hostname.endsWith('.preview.da.live') || url.hostname.endsWith('.stage-preview.da.live');
+
+  if (url.searchParams.get('dapreview') === 'on' || isPreviewHost) {
     return handleAEMProxyRequest({ req, env, daCtx });
   }
 
