e6b12804e43df8579ee7fc9e98e785f3a011cb44
Markus Haack <mhaack@adobe.com>
2026-02-06 11:03:04 -0800

    fix: image handling from content.da.live (#155)
    * fix: content.da.live image handling
    
    * fix: image handlich for content.da-live
    
    * fix: simpler code
    
    * chore: cleanup

---
+++ src/ue/rewrite-images.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { selectAll } from 'hast-util-select';
+
+const CONTENT_HOSTS = ['content.da.live', 'stage-content.da.live'];
+
+function stripContentPrefix(url, org, site) {
+  for (const host of CONTENT_HOSTS) {
+    const prefix = `https://${host}/${org}/${site}`;
+    if (url.startsWith(prefix)) {
+      return url.slice(prefix.length) || '/';
+    }
+  }
+  return null;
+}
+
+export default function rewrite(bodyTree, daCtx) {
+  const { org, site } = daCtx;
+  const elements = selectAll('img, picture > source', bodyTree);
+  const propByTag = { img: 'src', source: 'srcset' };
+  elements.forEach((el) => {
+    const prop = propByTag[el.tagName];
+    if (prop && el.properties[prop]) {
+      const rewritten = stripContentPrefix(el.properties[prop], org, site);
+      if (rewritten !== null) {
+        el.properties[prop] = rewritten;
+      }
+    }
+  });
+}
