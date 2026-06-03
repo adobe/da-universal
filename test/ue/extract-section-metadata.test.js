424bc011bdf9bbfc40257db2f1dfdfdd54c5dcf7
Markus Haack <mhaack@adobe.com>
2026-06-03 12:05:15 +0200

    fix: tests

--- test/ue/extract-section-metadata.test.js
+++ test/ue/extract-section-metadata.test.js
@@ -38,7 +38,7 @@ describe('extractSectionMetadata', () => {
   let toBlockCSSClassNames;
 
   before(async () => {
-    ({ default: extractSectionMetadata, toBlockCSSClassNames } = await esmock('../../src/ue/extract-section-metadata.js'));
+    ({ default: extractSectionMetadata, toBlockCSSClassNames } = await esmock('../../src/ue/section-metadata.js'));
   });
 
   describe('toBlockCSSClassNames', () => {
